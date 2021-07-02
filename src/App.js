6ef116749d0c5d605ca38dfe4220e296b7143e4d
Aditya Vardhan <cloverguy2001@gmail.com>
2021-07-02 11:17:02 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { InputLabel, Select, FormControl, MenuItem } from "@material-ui/core";
 import Header from "./components/Header";
+import InfoCard from "./components/InfoCard/InfoCard";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => setCountryInfo(data));
+  }, []);
 
   useEffect(() => {
     const getCountries = async () => {
@@ -25,9 +30,21 @@ function App() {
     getCountries();
   }, []);
 
-  const onCountryChange = (e) => {
+  const onCountryChange = async (e) => {
     const countryCode = e.target.value;
     setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
 
   return (
@@ -47,6 +64,23 @@ function App() {
               })}
             </Select>
           </FormControl>
+        </div>
+        <div className="status">
+          <InfoCard
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoCard
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoCard
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/InfoCard/InfoCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoCard.css";
+
+const InfoCard = ({ title, cases, total }) => {
+  return (
+    <Card className="info-card">
+      <CardContent>
+        <h1>{title}</h1>
+        <h2>{cases}</h2>
+        <h3>Total Cases : {total}</h3>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default InfoCard;
